81084e9714f4a50608c1c7e272315e3e1bc94f7f
benjamin perrin <perrin.benjamin77@gmail.com>
2022-09-21 11:15:38 +0200

    base modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crm';
+  public title: string;
+  // ou sinon public title!: string; si on l'innitialise pas ! = | undefined
+  constructor() {
+    this.title = 'crm';
+  }
 }
