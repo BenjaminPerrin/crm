9d7d26d4f34cd916f27003f4523b32507e57cdeb
benjamin perrin <perrin.benjamin77@gmail.com>
2022-09-21 16:26:44 +0200

    components in component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,21 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, NavComponent],
   imports: [CommonModule],
-  exports: [IconsModule, LoginModule, TemplatesModule, UiModule],
+  exports: [
+    IconsModule,
+    LoginModule,
+    TemplatesModule,
+    UiModule,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+  ],
 })
 export class CoreModule {}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ui2Component } from './components/ui2/ui2.component';
-import { UiComponent } from './ui/ui.component';
+import { UiComponent } from './components/ui/ui.component';
 
 @NgModule({
   declarations: [Ui2Component, UiComponent],
